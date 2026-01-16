2aee54501aae40e8280f3140bac9fee89ad1c332
stevenstank <stevenstank775@gmail.com>
2026-01-16 16:05:16 +0000

    learned something

---
+++ first.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+   int length;
+   int breadth;
+   printf("enter the value of length =");
+    scanf("%d",&length);
+    printf("enter the value of breadth =");
+   scanf("%d",&breadth);
+    printf("the area of rectangle is = %d",length * breadth);
+
+
+
+    return 0;
+
+}   
