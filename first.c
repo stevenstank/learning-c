281f536ea2d7ebbc477665f7e15a5b8a8fd7082f
stevenstank <stevenstank775@gmail.com>
2026-01-17 18:23:06 +0000

    strings

--- first.c
+++ first.c
@@ -1,15 +1,10 @@
 #include<stdio.h>
 
 int main(){
-   int length;
-   int breadth;
-   printf("enter the value of length =");
-    scanf("%d",&length);
-    printf("enter the value of breadth =");
-   scanf("%d",&breadth);
-    printf("the area of rectangle is = %d",length * breadth);
 
 
+char name[] = "saksham";
+printf("hello my name is %s\n", name);
 
     return 0;
 
