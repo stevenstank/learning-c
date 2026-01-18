0e8ee97ea84b14995a1508dbf5554f439bb9fa94
stevenstank <stevenstank775@gmail.com>
2026-01-18 11:48:37 +0000

    shopping cart program

--- first.c
+++ first.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 
 int main(){
-
-
-char name[] = "saksham";
-printf("hello my name is %s\n", name);
-
-    return 0;
-
+char food[10];
+float price;
+int quantity;
+printf("which item would yuu like to buy?: ");
+scanf("%s", food);
+printf("what is the price for each?:");
+scanf("%f", &price);
+printf("how many would you like to buy?:");
+scanf("%d", &quantity);
+printf("you have bought %d %s \n", quantity, food);
+printf("the total is %f \n", price*quantity);
+return 0;
 }   
