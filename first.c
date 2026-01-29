5e3e6f1bfa9e99a119801fefe8273e0a1deafccd
stevenstank <stevenstank775@gmail.com>
2026-01-29 17:56:51 +0000

    kl oops ka paper hai laadleeee

--- first.c
+++ first.c
@@ -2,40 +2,6 @@
 
 int main()
 {
-    char operator ='\0';
-    float num1 = 0.0;
-    float num2 = 0.0;
-    float result = 0.0;
-
-    printf("Enter the first numberr: ");
-    scanf("%f", &num1);
-    printf("Enter an operatorr (+, -, *, /): ");
-    scanf(" %c", &operator);
-    printf("Enter the second number: ");
-    scanf("%f", &num2);
-
-    switch (operator)
-        {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break; 
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0){
-                    result = num1 / num2;
-                } else {
-                    printf("Thora chutiya hai kya\n");
-                    
-                }
-                break;
-            default:
-                printf("Invalid operator\n");
-        }
-            printf("Result: %.2f\n", result);
+    printf("Hello, World!\n");
     return 0;
 }
